cc764f729c63e209cd16d61dae5c8841ed0c11e3
Marco Neumann <marco@crepererum.net>
2016-03-30 22:19:47 +0200

    cpp: vectorize query_dtw

--- cpp/src/query_dtw.cpp
+++ cpp/src/query_dtw.cpp
@@ -1,6 +1,7 @@
 #include <cstdint>
 
 #include <algorithm>
+#include <array>
 #include <fstream>
 #include <iostream>
 #include <iterator>
@@ -16,6 +17,15 @@
 #include <boost/iostreams/positioning.hpp>
 #include <boost/locale.hpp>
 #include <boost/program_options.hpp>
+
+// http://stackoverflow.com/a/10791845/1718219
+#define XSTR(x) STR(x)
+#define STR(x) #x
+
+#define SIMDPP_ARCH_X86_AVX2
+#define SIMDPP_NO_DISPATCHER
+#include <simdpp/simd.h>
+#pragma message "using vector size " XSTR(SIMDPP_FAST_FLOAT64_SIZE)
 
 #include "parser.hpp"
 #include "utils.hpp"
@@ -56,94 +66,207 @@ calc_t dist(calc_t a, calc_t b) {
     return d * d;
 }
 
-class dtw {
+template <unsigned N>
+simdpp::float64<N> dist(simdpp::float64<N> a, simdpp::float64<N> b) {
+    simdpp::float64<N> d = sub(a, b);
+    return mul(d, d);
+}
+
+template <typename T>
+class dtw_generic {
     public:
-        dtw(const calc_t* base, year_t ylength, std::size_t r) : _r(r), _r_plus(_r + 1), _r2(_r << 1), _r2_plus(_r2 + 1), _base(base), _ylength(ylength), _ylength_plus(_ylength + 1), _ylength_plus_minus_r(_ylength_plus - _r), _store_a(_r2_plus), _store_b(_r2_plus) {}
+        static constexpr std::size_t n = T::n;
 
-        calc_t calc(std::size_t i, std::size_t j) {
+        using internal_t      = typename T::internal_t;
+
+        dtw_generic(const calc_t* base, year_t ylength, std::size_t r) :
+            _r(r),
+            _r2(_r << 1),
+            _r2_plus(_r2 + 1),
+            _base(base),
+            _ylength(ylength),
+            _ylength_minus(_ylength - 1),
+            _ylength_minus_r(_ylength - _r),
+            _store_a(_r2_plus),
+            _store_b(_r2_plus) {}
+
+        internal_t calc(std::size_t i, std::size_t j0) {
             const calc_t* local_base_i = _base + (i * _ylength);
-            const calc_t* local_base_j = _base + (j * _ylength);
+            bases_t local_bases_j = T::get_bases(_base, _ylength, j0);
 
-            std::fill(_store_a.begin(), _store_a.end(), std::numeric_limits<calc_t>::infinity());
-            _store_a[_r] = 0.0;
+            std::fill(_store_a.begin(), _store_a.end(), T::infinity());
+            T::store(&_store_a[_r], T::zero());
 
             // remember: r <= ylength/2
 
-            for (std::size_t idx_i = 1; idx_i < _r_plus; ++idx_i) {
-                std::size_t idx_j_min = 1;
+            for (std::size_t idx_i = 0; idx_i < _r; ++idx_i) {
+                std::size_t idx_j_min = 0;
                 std::size_t idx_j_max = idx_i + _r;
 
-                inner_loop(local_base_i, local_base_j, idx_i, idx_j_min, idx_j_max, _r + 1 - idx_i);
+                inner_loop(local_base_i, local_bases_j, idx_i, idx_j_min, idx_j_max, _r - idx_i);
             }
 
-            for (std::size_t idx_i = _r_plus; idx_i < _ylength_plus_minus_r; ++idx_i) {
+            for (std::size_t idx_i = _r; idx_i < _ylength_minus_r; ++idx_i) {
                 std::size_t idx_j_min = idx_i - _r;
                 std::size_t idx_j_max = idx_i + _r;
 
-                inner_loop(local_base_i, local_base_j, idx_i, idx_j_min, idx_j_max, 0);
+                inner_loop(local_base_i, local_bases_j, idx_i, idx_j_min, idx_j_max, 0);
             }
 
-            for (std::size_t idx_i = _ylength_plus_minus_r; idx_i < _ylength_plus; ++idx_i) {
+            for (std::size_t idx_i = _ylength_minus_r; idx_i < _ylength; ++idx_i) {
                 std::size_t idx_j_min = idx_i - _r;
-                std::size_t idx_j_max = _ylength;  // = _ylength_plus - 1;
+                std::size_t idx_j_max = _ylength_minus;
 
-                inner_loop(local_base_i, local_base_j, idx_i, idx_j_min, idx_j_max, 0);
+                inner_loop(local_base_i, local_bases_j, idx_i, idx_j_min, idx_j_max, 0);
             }
 
-            return _store_a[_r];
+            return T::load(&_store_a[_r]);
         }
 
     private:
-        const std::size_t _r;
-        const std::size_t _r_plus;
-        const std::size_t _r2;
-        const std::size_t _r2_plus;
-        const calc_t* const _base;
-        const year_t _ylength;
-        const year_t _ylength_plus;
-        const std::size_t _ylength_plus_minus_r;
-        std::vector<calc_t> _store_a;
-        std::vector<calc_t> _store_b;
+        using bases_t         = typename T::bases_t;
+        using store_t         = std::vector<internal_t>;
+        using difference_type = typename store_t::difference_type;
 
-        void inner_loop(const calc_t* local_base_i, const calc_t* local_base_j, std::size_t idx_i, std::size_t idx_j_min, std::size_t idx_j_max, std::size_t store_delta) {
+        const std::size_t    _r;
+        const std::size_t   _r2;
+        const std::size_t   _r2_plus;
+        const calc_t* const _base;
+        const year_t        _ylength;
+        const year_t        _ylength_minus;
+        const std::size_t   _ylength_minus_r;
+        store_t             _store_a;
+        store_t             _store_b;
+
+        void inner_loop(const calc_t* local_base_i, bases_t local_bases_j, std::size_t idx_i, std::size_t idx_j_min, std::size_t idx_j_max, std::size_t store_delta) {
             std::size_t store_pos = store_delta;
-            calc_t current_value = std::numeric_limits<calc_t>::infinity();
+            internal_t current_value = T::infinity();
 
             std::fill(
                 _store_b.begin(),
-                _store_b.begin() + static_cast<std::iterator_traits<decltype(_store_b.begin())>::difference_type>(store_pos),
-                std::numeric_limits<calc_t>::infinity()
+                _store_b.begin() + static_cast<difference_type>(store_pos),
+                T::infinity()
             );
 
             for (std::size_t idx_j = idx_j_min; idx_j <= idx_j_max; ++idx_j) {
-                calc_t cost = dist(
-                    static_cast<calc_t>(local_base_i[idx_i]),
-                    static_cast<calc_t>(local_base_j[idx_j])
-                );
+                internal_t a = T::convert_single(local_base_i[idx_i]);
+                internal_t b = T::convert_multiple(local_bases_j, idx_j);
+                internal_t cost = dist(a, b);
 
-                calc_t dtw_insert = std::numeric_limits<calc_t>::infinity();
+                internal_t dtw_insert = T::infinity();
                 if (store_pos < _r2) {
                     // XXX: this is only illegal during the last round, might consider unrolling
-                    dtw_insert = _store_a[store_pos + 1];
+                    dtw_insert = T::load(&_store_a[store_pos + 1]);
                 }
-                calc_t dtw_delete = current_value;  // value from last round
-                calc_t dtw_match  = _store_a[store_pos];
+                internal_t dtw_delete = current_value;  // value from last round
+                internal_t dtw_match  = T::load(&_store_a[store_pos]);
 
-                current_value = cost + std::min(dtw_insert, std::min(dtw_delete, dtw_match));
-                _store_b[store_pos] = current_value;
+                current_value = cost + T::min3(dtw_insert, dtw_delete, dtw_match);
+                T::store(&_store_b[store_pos], current_value);
 
                 ++store_pos;
             }
 
             std::fill(
-                _store_b.begin() + static_cast<std::iterator_traits<decltype(_store_b.begin())>::difference_type>(store_pos),
+                _store_b.begin() + static_cast<difference_type>(store_pos),
                 _store_b.end(),
-                std::numeric_limits<calc_t>::infinity()
+                T::infinity()
             );
 
             std::swap(_store_a, _store_b);
         }
 };
+
+struct dtw_impl_simple {
+    static constexpr std::size_t n = 1;
+
+    using internal_t = calc_t;
+    using bases_t    = const calc_t*;
+
+    inline static internal_t convert_single(calc_t x) {
+        return static_cast<calc_t>(x);
+    }
+
+    inline static internal_t convert_multiple(bases_t bases, std::size_t idx) {
+        return static_cast<calc_t>(bases[idx]);
+    }
+
+    inline static bases_t get_bases(const calc_t* base, std::size_t ylength, std::size_t j0) {
+        return base + (j0 * ylength);
+    }
+
+    inline static internal_t load(internal_t* ptr) {
+        return *ptr;
+    }
+
+    inline static internal_t min3(internal_t a, internal_t b, internal_t c) {
+        return std::min(a, std::min(b, c));
+    }
+
+    inline static internal_t infinity() {
+        return std::numeric_limits<calc_t>::infinity();
+    }
+
+    inline static void store(internal_t* ptr, internal_t data) {
+        *ptr = data;
+    }
+
+    inline static internal_t zero() {
+        return 0.0;
+    }
+};
+
+struct dtw_impl_vectorized {
+    static constexpr std::size_t n = SIMDPP_FAST_FLOAT64_SIZE;
+
+    using internal_t = simdpp::float64<n>;
+    using bases_t    = std::array<const calc_t*, n>;
+
+    inline static internal_t convert_single(calc_t x) {
+        return simdpp::make_float(x);
+    }
+
+    inline static internal_t convert_multiple(bases_t bases, std::size_t idx) {
+        return simdpp::make_float(
+            std::get<0>(bases)[idx],
+            std::get<1>(bases)[idx],
+            std::get<2>(bases)[idx],
+            std::get<3>(bases)[idx]
+        );
+    }
+
+    inline static bases_t get_bases(const calc_t* base, std::size_t ylength, std::size_t j0) {
+        return {{
+            (base + ((j0 + 0) * ylength)),
+            (base + ((j0 + 1) * ylength)),
+            (base + ((j0 + 2) * ylength)),
+            (base + ((j0 + 3) * ylength))
+        }};
+    }
+
+    inline static internal_t load(internal_t* ptr) {
+        return simdpp::load(ptr);
+    }
+
+    inline static internal_t min3(internal_t a, internal_t b, internal_t c) {
+        return simdpp::min(a, simdpp::min(b, c));
+    }
+
+    inline static internal_t infinity() {
+        return simdpp::make_float(std::numeric_limits<double>::infinity());
+    }
+
+    inline static void store(internal_t* ptr, internal_t data) {
+        simdpp::store(ptr, data);
+    }
+
+    inline static internal_t zero() {
+        return simdpp::make_float(0.0);
+    }
+};
+
+using dtw_simple     = dtw_generic<dtw_impl_simple>;
+using dtw_vectorized = dtw_generic<dtw_impl_vectorized>;
 
 
 int main(int argc, char** argv) {
@@ -213,10 +336,26 @@ int main(int argc, char** argv) {
 
     std::size_t usable_limit = std::min(limit, n);
     std::vector<std::pair<std::size_t, calc_t>> distances(n);
-    dtw mydtw(base, ylength, r);
-    for (std::size_t j = 0; j < n; ++j) {
-        distances[j] = std::make_pair(j, mydtw.calc(i, j));
+    std::size_t n_over = n % static_cast<std::size_t>(4);
+    std::size_t n_good = n - n_over;
+
+    dtw_vectorized mydtw_vectorized(base, ylength, r);
+    for (std::size_t j = 0; j < n_good; j += dtw_vectorized::n) {
+        auto v_results = mydtw_vectorized.calc(i, j);
+
+        std::array<double, dtw_vectorized::n> d_results;
+        simdpp::store(&d_results, v_results);
+
+        for (std::size_t idx = 0; idx < dtw_vectorized::n; ++idx) {
+            distances[j + idx] = std::make_pair(j + idx, d_results[idx]);
+        }
     }
+
+    dtw_simple mydtw_simple(base, ylength, r);
+    for (std::size_t j = n_good; j < n; ++j) {
+        distances[j] = std::make_pair(j, mydtw_simple.calc(i, j));
+    }
+
     std::partial_sort(
         distances.begin(),
         distances.begin() + static_cast<std::iterator_traits<decltype(distances.begin())>::difference_type>(usable_limit),
@@ -225,9 +364,12 @@ int main(int argc, char** argv) {
             return a.second < b.second;
         }
     );
+
     for (std::size_t j = 0; j < usable_limit; ++j) {
         std::cout
             << boost::locale::conv::utf_to_utf<char>(idxmap[distances[j].first])
+            << " @ "
+            << distances[j].first
             << " : "
             << distances[j].second
             << std::endl;
