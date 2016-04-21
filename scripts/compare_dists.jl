248bc826addfe3d6e0d4d58c8a1d4126d814aecc
Marco Neumann <marco@crepererum.net>
2016-04-21 19:15:04 +0200

    scripts: implement wavelet compression DTW regression test whatever

---
+++ scripts/compare_dists.jl
@@ -0,0 +1,31 @@
+#!/usr/bin/env julia
+
+fmap = ARGS[1]
+fd1  = ARGS[2]
+fd2  = ARGS[3]
+
+include("../julia/lib.jl")
+
+map_i2s, map_s2i = loadmap(fmap)
+n = length(map_i2s)
+
+fp1 = open(fd1)
+fp2 = open(fd2)
+
+data1 = Mmap.mmap(fp1, Matrix{Float64}, (n, 1))
+data2 = Mmap.mmap(fp2, Matrix{Float64}, (n, 1))
+
+delta = data2 - data1
+
+@printf "median: %.5f\n" median(delta)
+@printf "mean:   %.5f\n" mean(delta)
+@printf "std:    %.5f\n" std(delta)
+@printf "q0.02:  %.5f\n" quantile(delta[:,1], 0.02)
+@printf "q0.09:  %.5f\n" quantile(delta[:,1], 0.09)
+@printf "q0.25:  %.5f\n" quantile(delta[:,1], 0.25)
+@printf "q0.75:  %.5f\n" quantile(delta[:,1], 0.75)
+@printf "q0.91:  %.5f\n" quantile(delta[:,1], 0.91)
+@printf "q0.98:  %.5f\n" quantile(delta[:,1], 0.98)
+
+close(fp1)
+close(fp2)
---
+++ scripts/run_wavelet_compression_test.sh
@@ -0,0 +1,90 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"/..
+
+ngram=$1
+maxerror=$2
+
+tmpdir=$dir/tmp
+datadir=$dir/data
+scriptdir=$dir/scripts
+binarydir=$dir/cpp/build
+
+ylength=256
+dtw_r=10
+indexsize=$(expr 1024 \* 1024 \* 1024 \* 4)
+inputfile=$datadir/v0-forwavelet.data
+mapfile=$datadir/final.map
+reffile=$datadir/ref-dists.datax
+reportfile=$dir/wavelet_compression_test.report
+
+logfile=$tmpdir/wavelet_compression_test.log
+indexfile=$tmpdir/wavelet.index
+dumpfile=$tmpdir/dump.data
+dtwinfile=$tmpdir/dtwin.data
+distfile=$tmpdir/dist.datax
+
+echo "prepare..."
+mkdir -p $tmpdir
+echo "done"
+
+echo "write intro to report..."
+echo "========== BEGIN ==========" >> $reportfile
+echo "ngram=$ngram" >> $reportfile
+echo "maxerror=$maxerror" >> $reportfile
+echo "done"
+
+echo "run wavelet compression..."
+LD_PRELOAD=/usr/lib/libtcmalloc.so $binarydir/index_wavelet \
+    --binary $inputfile \
+    --error $maxerror \
+    --index $indexfile \
+    --map $mapfile \
+    --size $indexsize \
+    --ylength $ylength >> $logfile
+grep "level=" $logfile >> $reportfile
+echo "done"
+
+echo "dump compressed data back into plain file..."
+$binarydir/dump_wavelet_index \
+    --binary $dumpfile \
+    --index $indexfile \
+    --map $mapfile \
+    --ylength $ylength >> $logfile
+echo "done"
+
+echo "prepare data for DTW..."
+$scriptdir/transform_data.jl \
+    $mapfile \
+    $dumpfile \
+    $dtwinfile \
+    transform_gradient_smooth1 \
+    Float64 >> $logfile
+echo "done"
+
+echo "calc DTW distances..."
+$binarydir/calc_dtw_simple \
+    --binary $dtwinfile \
+    --map $mapfile \
+    --output $distfile \
+    --query $ngram \
+    --r $dtw_r \
+    --ylength $ylength >> $logfile
+echo "done"
+
+echo "compare results..."
+$scriptdir/compare_dists.jl \
+    $mapfile \
+    $distfile \
+    $reffile | tee --append $logfile $reportfile > /dev/null
+echo "done"
+
+echo "write outro to report..."
+echo "=========== END ===========" >> $reportfile
+echo "done"
+
+echo "clean up..."
+rm $logfile $indexfile $dumpfile $dtwinfile $distfile
+echo "done"
