04cea561eaa11082675ffa14b39b138ad8263a70
Marco Neumann <marco@crepererum.net>
2016-05-22 22:24:14 +0200

    scripts.compare_dists: add dynamic top-n comparison

--- scripts/compare_dists.jl
+++ scripts/compare_dists.jl
@@ -1,5 +1,7 @@
 #!/usr/bin/env julia
 
+using StringDistances
+
 fmap = ARGS[1]
 fd1  = ARGS[2]
 fd2  = ARGS[3]
@@ -38,6 +40,47 @@ delta = (data2 - data1) ./ map(zero2one, data1)
 @printf "q0.91:  %.5f\n" quantile(delta[:,1], 0.91)
 @printf "q0.98:  %.5f\n" quantile(delta[:,1], 0.98)
 @printf "max:    %.5f\n" maximum(delta)
+
+
+# dynamic limit calculation
+function get_best_idx(sorted)
+    data = collect(map(x -> x[2], sorted[2:convert(Int64, round(length(sorted) / 2))]))
+    normalized = data - data[1]
+    grad = gradient(normalized) ./ normalized
+    grad2 = gradient(grad)
+    indmax(grad2) # - 1 + 1
+end
+sorted1 = sort(collect(enumerate(data1[:, 1])), by=x -> x[2])
+sorted2 = sort(collect(enumerate(data2[:, 1])), by=x -> x[2])
+limit1 = get_best_idx(sorted1)
+limit2 = get_best_idx(sorted2)
+limit = limit1
+@printf "limit1:  %i (<< choosen)\n" limit1
+@printf "limit2:  %i\n" limit2
+
+# string comparison
+counter = 0
+known = Dict{Int64, Int64}()
+for entry in sorted1[1:limit]
+    if !haskey(known, entry[1])
+        known[entry[1]] = counter
+        counter += 1
+    end
+end
+newentries = 0
+for entry in sorted2[1:limit]
+    if !haskey(known, entry[1])
+        known[entry[1]] = counter
+        counter += 1
+        newentries += 1
+    end
+end
+string1 = join(map(x -> Char(known[x[1]]), sorted1[1:limit]))
+string2 = join(map(x -> Char(known[x[1]]), sorted2[1:limit]))
+
+@printf "sdiff:  %.5f\n" (1 - compare(Levenshtein(), string1, string2))
+@printf "enew:   %.5f\n" (newentries / limit)
+
 
 close(fp1)
 close(fp2)
